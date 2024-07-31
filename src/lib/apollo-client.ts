2610c9899bc9e666163162dba0308034b555daaf
Roman Smolin <romasmolin29@gmail.com>
2024-07-31 22:07:51 +0300

    FIX

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,13 +2,13 @@ import { onError } from '@apollo/client/link/error';
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-const STRAPI_URL = process.env.STRAPI_URL || 'http://localhost:1337';
+const STRAPI_URL = process.env.GRAPHQL_API_URL || 'http://localhost:1337';
 
 export const { getClient } = registerApolloClient(() => {
     return new ApolloClient({
         cache: new InMemoryCache(),
         link: new HttpLink({
-            uri: `${STRAPI_URL}/graphql`,
+            uri: process.env.GRAPHQL_API_URL || 'http://localhost:1337/graphql',
         }),
     });
 });
