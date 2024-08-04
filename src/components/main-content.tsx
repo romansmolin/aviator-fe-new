14a988d32b4428028a7bfdb6771826f9b70dc1c1
Romans Smolins <romans.smolins@fintecor.com>
2024-08-04 21:51:22 +0300

    Ui update

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -6,7 +6,7 @@ interface MainContentProps {
 
 const MainContent:React.FC<MainContentProps> = ({ children }) => {
     return (
-        <main className="mt-14 w-screen lg:w-[unset] flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 ">
+        <main className="mt-14 w-screen lg:w-[unset] flex flex-1 flex-col gap-4 md:p-4 lg:gap-6 lg:p-6 ">
             {/* <div className="flex items-center">
                 <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
             </div> */}
