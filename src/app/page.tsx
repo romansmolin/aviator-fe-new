321aa97e7654aeec400be01b9a25cd44e5683513
Roman Smolin <romasmolin29@gmail.com>
2024-08-10 18:06:15 +0300

    FIX HOME PAGE LOADING

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 import Loading from "@/components/loading";
+import { getServerQuery } from "@/lib/apollo-client";
+import { CASINO_TOP_BY_COUNTRY } from "@/shared/schemas/casinos";
 import Home from "@/views/Home/home";
 import { Suspense } from "react";
 
-export default function HomePage() {
-  return  <Home />
-  
+export default async function HomePage() {
+	const variables = {
+		country: "Germany",
+	}
+
+	const { getTopByCountryName } = await getServerQuery(CASINO_TOP_BY_COUNTRY, variables)
+	
+	return (
+		<Suspense fallback={<Loading />}>
+			<Home topCasinos={getTopByCountryName} />
+		</Suspense>
+	)
+
 }
--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -1,22 +1,17 @@
-import { Suspense } from "react";
 import BestCasinosMonthly from "./blocks/best-casinos-monthly";
 import HeroSection from "./blocks/hero-section";
-import { getServerQuery } from "@/lib/apollo-client";
-import { CASINO_TOP_BY_COUNTRY } from "@/shared/schemas/casinos";
-import Loading from "@/components/loading";
 
-export default async function Home() {
-    const variables = {
-        country: "Germany",
-    }
-    const { getTopByCountryName } = await getServerQuery(CASINO_TOP_BY_COUNTRY, variables)
+interface HomePageProps {
+    topCasinos: any
+}
 
+const Home: React.FC<HomePageProps> = ({ topCasinos }) => {
     return (
         <>
             <HeroSection />
-            <Suspense fallback={<Loading />}>
-                <BestCasinosMonthly casinos={getTopByCountryName.top_list} />
-            </Suspense>
+            <BestCasinosMonthly casinos={topCasinos.top_list} />
         </>
-    )
-}
+    );
+};
+
+export default Home;
