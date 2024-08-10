980f2866b90cd13acef8e32071ab98ed544d34e0
Roman Smolin <romasmolin29@gmail.com>
2024-08-10 22:44:33 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Loading from "@/components/loading";
-import { getServerQuery } from "@/lib/apollo-client";
-import { CASINO_TOP_BY_COUNTRY } from "@/shared/schemas/casinos";
 import Home from "@/views/Home/home";
 import { Suspense } from "react";
 
-export default async function HomePage() {
+export default function HomePage() {
 
 	return (
 		<Suspense fallback={<Loading />}>
--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -2,6 +2,7 @@ import { getServerQuery } from "@/lib/apollo-client";
 import BestCasinosMonthly from "./blocks/best-casinos-monthly";
 import HeroSection from "./blocks/hero-section";
 import { CASINO_TOP_BY_COUNTRY } from "@/shared/schemas/casinos";
+import Loading from "@/components/loading";
 
 const Home = async () => {
 
