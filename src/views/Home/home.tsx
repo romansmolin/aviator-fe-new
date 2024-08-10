9c54ea523f57c55cd4538adf75ed175b33c7990b
Roman Smolin <romasmolin29@gmail.com>
2024-08-10 17:47:02 +0300

    Fix

--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -1,7 +1,9 @@
+import { Suspense } from "react";
 import BestCasinosMonthly from "./blocks/best-casinos-monthly";
 import HeroSection from "./blocks/hero-section";
 import { getServerQuery } from "@/lib/apollo-client";
 import { CASINO_TOP_BY_COUNTRY } from "@/shared/schemas/casinos";
+import Loading from "@/components/loading";
 
 export default async function Home() {
     const variables = {
@@ -10,9 +12,9 @@ export default async function Home() {
     const { getTopByCountryName } = await getServerQuery(CASINO_TOP_BY_COUNTRY, variables)
 
     return (
-        <>
+        <Suspense fallback={<Loading />}>
             <HeroSection />
             <BestCasinosMonthly casinos={getTopByCountryName.top_list} />
-        </>
+        </Suspense>
     )
 }
